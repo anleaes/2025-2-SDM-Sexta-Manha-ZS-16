f2163b6f17a60785d985ceb94591bba170173180
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 10:36:50 -0300

    Implementação inicial da lógica do modelo Buildings

--- apps/buildings/models.py
+++ apps/buildings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Building(models.Model):
+    name = models.CharField('Nome', max_length=150)
+    address = models.CharField('Endereço', max_length=255)
+    description = models.TextField('Descrição', blank=True, null=True)
+    operational_status = models.CharField('Status Operacional', max_length=50)
