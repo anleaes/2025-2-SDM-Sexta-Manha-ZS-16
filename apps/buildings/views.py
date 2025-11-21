88ddb9fcec70f5e1d2a6e75dbc3dd755f61e0d16
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 11:31:05 -0300

    Implementação inicial da lógica de views do app buildings

--- apps/buildings/views.py
+++ apps/buildings/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Building
+from .serializers import BuildingSerializer
 
 # Create your views here.
