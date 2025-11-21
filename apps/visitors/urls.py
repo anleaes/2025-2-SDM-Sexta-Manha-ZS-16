06e01a4e16bad04665fa2bfc8f3ac2484f834f75
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 04:04:56 -0300

    Implementação da lógica de urls para o app visitors

--- apps/visitors/urls.py
+++ apps/visitors/urls.py
@@ -2,3 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import VisitorViewSet
 from . import views
+
+app_name = 'visitors'
+
+router = routers.DefaultRouter()
+router.register('visitors', VisitorViewSet, basename='visitantes')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
