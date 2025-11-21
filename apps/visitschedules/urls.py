a8dae56f80ac96a342da1a41336ce3d1e8da033d
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 06:02:39 -0300

    Implementação do arquivo urls do app visitschedule

--- apps/visitschedules/urls.py
+++ apps/visitschedules/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import VisitScheduleViewSet
 
+app_name = 'visitschedules'
+
+router = routers.DefaultRouter()
+router.register('', VisitScheduleViewSet, basename='agendamento-de-visita')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
