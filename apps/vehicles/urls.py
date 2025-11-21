d5d1476674abaa03e1e7bd033cbee1fad5988f4a
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 05:05:25 -0300

    Implementação do arquivo urls do app vehicles

--- apps/vehicles/urls.py
+++ apps/vehicles/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import VehicleViewSet
+
+app_name = 'vehicles'
+
+router = routers.DefaultRouter()
+router.register('', views.ProductViewSet, basename='veículos')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
