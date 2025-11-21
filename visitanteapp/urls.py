cd3402dad3923b74a1e1579f92bcb1aeea1b3081
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 05:09:03 -0300

    Adição do caminho da entidade Vehicles no arquivo urls principal

--- visitanteapp/urls.py
+++ visitanteapp/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('visitantes/', include('visitors.urls', namespace='visitors')),
+    path('veículos/', include('vehicles.urls', namespace='vehicles')),
+
 ]
