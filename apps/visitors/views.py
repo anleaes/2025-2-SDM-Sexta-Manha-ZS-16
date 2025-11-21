e20ae99d431ffdb841b672c44344baddc8b5b48b
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 03:54:11 -0300

    Implementação do views de Visitors

--- apps/visitors/views.py
+++ apps/visitors/views.py
@@ -4,3 +4,6 @@ from .models import Category
 from .serializer import CategorySerializer
 
 # Create your views here.
+class VisitorViewSet(viewsets.ModelViewSet):
+    queryset = Visitor.objects.all().order_by('id')
+    serializer_class = VisitorSerializer
