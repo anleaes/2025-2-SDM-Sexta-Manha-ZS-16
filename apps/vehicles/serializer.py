83e220d61d0923264eabb86f695d7604443747e1
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 04:55:32 -0300

    Implementação dos serializers do app vehicles

--- apps/vehicles/serializer.py
+++ apps/vehicles/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Vehicle
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
