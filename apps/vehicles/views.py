dcce0146d2479f2f901177139c9c6173ea95810b
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 04:59:54 -0300

    Implementação do views do app vehicles e alteração nas importações do arquivo

--- apps/vehicles/views.py
+++ apps/vehicles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from .models import Vehicle
 from .serializers import VehicleSerializer
 
 # Create your views here.
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all().order_by('id')
+    serializer_class = VehicleSerializer
