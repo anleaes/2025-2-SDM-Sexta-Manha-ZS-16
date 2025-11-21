d091f25e83ce73b902dbe9830c2aefeb53649281
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 05:54:36 -0300

    Importações iniciais no arquivo views do app visitschedule

--- apps/visitschedules/views.py
+++ apps/visitschedules/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import VisitSchedule
+from .serializer import VisitScheduleSerializer
 
 # Create your views here.
