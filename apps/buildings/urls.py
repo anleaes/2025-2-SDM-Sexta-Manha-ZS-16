dd0c6235fcf594224c5e3a288917d8351b00bfa7
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 11:39:13 -0300

    Implementação do arquivo urls do app building

--- apps/buildings/urls.py
+++ apps/buildings/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import BuildingViewSet
+
+app_name = 'buildings'
+
+router = routers.DefaultRouter()
+router.register('', BuildingViewSet, basename='prédios')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
