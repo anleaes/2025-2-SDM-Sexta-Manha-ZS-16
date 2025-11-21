6220dde1fdbb378c1e5ed14195d7bdee2240f880
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 04:58:03 -0300

    Importações iniciais no arquivo view.py do app vehicles

--- apps/vehicles/views.py
+++ apps/vehicles/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Vehicle
+from .serializers import VehicleSerializer
 
 # Create your views here.
