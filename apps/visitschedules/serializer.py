b3e2616163afce0afadbaca7565c5a0457947e3c
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 05:45:21 -0300

    implementação dos serializers do app visitschedule

--- apps/visitschedules/serializer.py
+++ apps/visitschedules/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import VisitSchedule
+
+class VisitScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VisitSchedule
+        fields = '__all__'
