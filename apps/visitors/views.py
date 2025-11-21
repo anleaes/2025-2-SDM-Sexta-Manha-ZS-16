93a50b96f98dcdba15f5f74197d26acafa2cb807
Rafael Girardi <rafaelgi200418@gmail.com>
2025-11-21 04:30:28 -0300

    Fix: Resolução de erros nos imports de views e urls

--- apps/visitors/views.py
+++ apps/visitors/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Visitor
-from .serializers import VisitorSerializer
+from .serializer import VisitorSerializer
 
 # Create your views here.
 class VisitorViewSet(viewsets.ModelViewSet):
